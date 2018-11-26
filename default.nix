4f21382327e54e24c37900ff12c0e3651cb519a3
Richard Marko <srk@48.io>
2018-11-26 22:03:45 +0100

    dontCheck for genvalidity-time

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc843" }:
 let
   scylla = nixpkgs.haskell.packages.${compiler}.callPackage ./data-scylla.nix {};
-  pretty-relative-time = nixpkgs.haskell.packages.${compiler}.callPackage ./pretty-relative-time.nix {};
+  # testsuite fails due to no Ord for ZonedTime :X
+  genvalidity-time = nixpkgs.haskell.lib.dontCheck nixpkgs.haskell.packages.${compiler}.genvalidity-time;
+  pretty-relative-time = nixpkgs.haskell.packages.${compiler}.callPackage ./pretty-relative-time.nix { inherit genvalidity-time; };
 in
 nixpkgs.haskell.packages.${compiler}.callPackage ./scylla-api.nix { inherit scylla pretty-relative-time; }
